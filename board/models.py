f17b290351b0ea4cf6bfd85b9102b34baf0d5f64
akahard2dj <akahard2dj@naver.com>
2017-03-13 17:14:14 +0900

    Update models.py

--- board/models.py
+++ board/models.py
@@ -9,8 +9,18 @@ class Category(models.Model):
         return self.name
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
 class Post(models.Model):
     category = models.ForeignKey(Category)
+    user = models.ForeignKey(UserProfile, null=True)
     title = models.CharField(max_length=200)
     content = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,10 +31,4 @@ class Post(models.Model):
         return self.title
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
 
-    website = models.URLField(blank=True)
-
-    def __str__(self):
-        return self.user.username
