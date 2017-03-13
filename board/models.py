34f538416f37e9671b392015ec6e598bade23c1e
akahard2dj <akahard2dj@naver.com>
2017-03-13 16:20:25 +0900

    Update models.py
    PEP

--- board/models.py
+++ board/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class Post(models.Model):
     category = models.ForeignKey(Category)
