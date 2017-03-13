a18398838957d84710bf156fd3421ff34546471c
akahard2dj <akahard2dj@naver.com>
2017-03-13 17:14:35 +0900

    Update forms.py

--- board/forms.py
+++ board/forms.py
@@ -10,7 +10,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        exclude = ('category', 'views', )
+        exclude = ('category', 'views', 'user', )
 
 
 class UserForm(forms.ModelForm):
