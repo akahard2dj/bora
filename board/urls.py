e5375190a212776dd6dc3c43cd1b59c30891abe0
akahard2dj <akahard2dj@naver.com>
2017-03-13 16:21:31 +0900

    Update urls.py
    updating catogory url

--- board/urls.py
+++ board/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^category/(?P<category_name>[\w]+)/$', views.category, name='category'),
     url(r'^category/(?P<category_name>[\w]+)/add_post/$', views.add_post, name='add_post'),
+    url(r'^category/(?P<category_name>[\w]+)/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
